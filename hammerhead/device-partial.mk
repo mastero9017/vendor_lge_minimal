2a74d6e81065c4ec63994563e26cbd2804890a4c
mastero9017 <mastero9017@gmail.com>
2015-10-30 23:30:59 +0100

    Add libfmas

--- hammerhead/device-partial.mk
+++ hammerhead/device-partial.mk
@@ -211,4 +211,5 @@ PRODUCT_COPY_FILES := \
     vendor/lge/hammerhead/proprietary/libWVStreamControlAPI_L1.so:system/vendor/lib/libWVStreamControlAPI_L1.so:qcom \
     vendor/lge/hammerhead/proprietary/bcm2079x-b5_firmware.ncd:system/vendor/firmware/bcm2079x-b5_firmware.ncd:broadcom \
     vendor/lge/hammerhead/proprietary/bcm2079x-b5_pre_firmware.ncd:system/vendor/firmware/bcm2079x-b5_pre_firmware.ncd:broadcom \
-    vendor/lge/hammerhead/proprietary/bcm4335c0.hcd:system/vendor/firmware/bcm4335c0.hcd:broadcom
+    vendor/lge/hammerhead/proprietary/bcm4335c0.hcd:system/vendor/firmware/bcm4335c0.hcd:broadcom \
+    vendor/lge/hammerhead/proprietary/libfmas.so:system/lib/soundfx/libfmas.so
